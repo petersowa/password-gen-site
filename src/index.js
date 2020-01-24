875c068e56b5d6795bb6d632e557e25391ce373f
petersowa <petersowa@users.noreply.github.com>
2020-01-23 22:34:44 -0600

    add console logs for app name and version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/AppRoutes.js'
+import App from './components/AppRoutes.js';
 
+console.log(`App Name: ${process.env.REACT_APP_NAME}`);
+console.log(`App Version: ${process.env.REACT_APP_VERSION}`);
 
 render(<App />, document.getElementById('root'));
