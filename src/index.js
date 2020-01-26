6f714f9911ff850f0eebc842ebf683e2c4e8f1e3
petersowa <petersowa@users.noreply.github.com>
2020-01-25 22:23:54 -0600

    cleanup code - remove logs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/AppRoutes.js';
 
-console.log(`App Name: ${process.env.REACT_APP_NAME}`);
-console.log(`App Version: ${process.env.REACT_APP_VERSION}`);
+// console.log(`App Name: ${process.env.REACT_APP_NAME}`);
+// console.log(`App Version: ${process.env.REACT_APP_VERSION}`);
 
 render(<App />, document.getElementById('root'));
